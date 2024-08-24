a135afe49029b48df7b197dbf2d2a066a192d829
nbow2 <nmawg22@gmail.com>
2024-08-24 05:04:25 +0300

    Finished Search Screen Design . Added Movie Details widget.

--- lib/search/widgets/searched_Details.dart
+++ lib/search/widgets/searched_Details.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 
-class SearchedDetails extends StatelessWidget {
-  const SearchedDetails({super.key});
+/*
+TODO this widget is NOT COMPLETED SO please wait
+*/
+/*
+ TODO ONLY Prototype
+*/
 
+class MovieDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: Colors.transparent,
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                'None', // Replace image URL
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(width: 10),
+
+              const Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Alita Battle Angel',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+
+                    Text('2019'),
+                    SizedBox(height: 10),
+                    Text('Rosa Salazarrr'),
+
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          const Divider(
+            color: Colors.white,
+            thickness: 1,
+            height: 20,
+          ),
+        ],
+      ),
+    );
   }
 }
