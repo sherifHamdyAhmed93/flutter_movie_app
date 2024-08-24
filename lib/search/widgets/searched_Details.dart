02009fdb240eefe41f415d908a9fe39e73e0f841
nbow2 <nmawg22@gmail.com>
2024-08-24 04:38:03 +0300

    Finished Search Screen Design .

---
+++ lib/search/widgets/searched_Details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchedDetails extends StatelessWidget {
+  const SearchedDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
